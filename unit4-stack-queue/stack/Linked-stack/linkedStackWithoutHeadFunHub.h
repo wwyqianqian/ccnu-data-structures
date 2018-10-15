fc500c6243a511a119bfb00c4a3a461b1302b26b
wwyqianqian <1208331933@qq.com>
2018-10-16 00:18:24 +0800

    [feat]: linkedStackWithoutHeadFunHub.h

---
+++ unit4-stack-queue/stack/Linked-stack/linkedStackWithoutHeadFunHub.h
@@ -0,0 +1,113 @@
+#ifndef LINKED_STACK_WITHOUT_HEAD_FUN_HUB_H
+#define LINKED_STACK_WITHOUT_HEAD_FUN_HUB_H
+using namespace std;
+
+typedef int SElemType;
+typedef struct LNode {
+    SElemType data; // data
+    struct LNode *next; // pointer
+} LNode, *LinkList;
+
+typedef LinkList LinkStack;
+
+
+// Function1: Initialize a linked stack
+void InitStack(LinkStack &S) {
+//    LNode *S = new LNode;
+//    S->data = NULL; // temporary
+//    S->next = NULL;
+    S = nullptr;
+}
+
+
+// Function2: Destroy the stack
+void DestroyStack(LinkStack &S) {
+    LNode *p;
+    while (S != nullptr) {
+        p = S->next;
+        delete S;
+        S = p;
+    }
+}
+
+
+// Function3: Clear the existing stack
+void ClearStack(LinkStack &S) {
+    LNode *p;
+    while (S->next != nullptr) {
+        p = S->next;
+        S->next = p->next;
+        delete p;
+    }
+    S->data = nullptr;
+}
+
+
+// Function4: Return TRUE if the Stack is empty
+bool StackEmpty(LinkStack S) {
+    return !S;
+}
+
+
+// Function5: Return the number of elements
+int StackLength(LinkStack S) {
+    if (S->data == NULL && S->next == nullptr) {
+        return 0;
+    }
+    LNode *p;
+    p = S;
+
+    int cou = 0;
+    while (p != NULL) {
+        cou++;
+        p = p->next;
+    }
+    return cou;
+}
+
+
+// Function6: Get the top element of the stack, store it as e
+bool GetTop(LinkStack S, SElemType &e) {
+    if (!S) {
+        return false;
+    } else {
+        e = S->data;
+        return true;
+    }
+}
+
+
+// Function7: Push an element 'e' to the stack
+void Push(LinkStack &S, SElemType e) {
+    LNode *p = new LNode;
+    p->data = e;
+    p->next = S;
+    S = p;
+}
+
+
+// Function8: Pop the top element of the existing stack, store it as e
+bool Pop(LinkStack &S, SElemType &e) {
+    if (S) {
+        LNode *p = S;
+        S = S->next;
+        e = p->data;
+        delete p;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// Function9: Pop every element of the existing stack
+bool StackTraverse(LinkStack S) {
+    LNode *p = S;
+    if (p->data != NULL) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+}
+
+
+#endif
